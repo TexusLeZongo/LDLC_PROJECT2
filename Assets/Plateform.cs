59fdd39635b0fcd60ec3f263328b2e42e0e26a28
DESKTOP-4D3TF2O\texus <texus690@gmail.com>
2020-11-26 14:04:30 +0100

    premier commit

---
+++ Assets/Plateform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plateform : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    Vector3 direction = new Vector3(0f, 0f, 1f);
+    void FixedUpdate()
+    {
+        Vector3 currentPosition = transform.position;
+        
+        float speed = 2f;
+
+        if(transform.position.z >= 4.5f)
+        {
+           direction = new Vector3(0f, 0f, -1f);
+        }
+       
+        Vector3 newPosition = currentPosition + direction * speed * Time.deltaTime;
+
+        rigidbody.MovePosition(newPosition);
+
+    }
+
+
+}
